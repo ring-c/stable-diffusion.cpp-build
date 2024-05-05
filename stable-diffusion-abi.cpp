e810509eeadbecadf227e795fc25d63d90623c80
Eugene Denisenko <caravus@craft.sc>
2024-05-06 03:17:32 +0500

    upscale

--- stable-diffusion-abi.cpp
+++ stable-diffusion-abi.cpp
@@ -3,36 +3,6 @@
 #include "util.h"
 #include <string>
 
-uint8_t *get_image_data(const sd_image_t *images, int index) {
-    return images[index].data;
-}
-
-uint32_t get_image_width(const sd_image_t *images, int index) {
-    return images[index].width;
-}
-
-uint32_t get_image_height(const sd_image_t *images, int index) {
-    return images[index].height;
-}
-
-uint32_t get_image_channel(const sd_image_t *images, int index) {
-    return images[index].channel;
-}
-
-void sd_images_free(const sd_image_t *images) {
-    if (images != nullptr) {
-        delete[]images;
-    }
-    images = nullptr;
-}
-
-void sd_image_free(sd_image_t *image) {
-    if (image != nullptr) {
-        delete image;
-    }
-    image = nullptr;
-}
-
 sd_image_t *upscale_go(
         upscaler_ctx_t *ctx,
         uint32_t upscale_factor,
@@ -54,7 +24,7 @@ sd_image_t *upscale_go(
             .channel=channel,
             .data=data,
     };
-    
+
     output_image[0] = upscale(ctx, input_image, upscale_factor);
     return output_image;
 }
@@ -79,62 +49,4 @@ sd_ctx_t *new_sd_ctx_go(new_sd_ctx_go_params *context_params) {
             context_params->keep_control_net_cpu,
             context_params->keep_vae_on_cpu
     );
-}
-
-new_sd_ctx_go_params *new_sd_ctx_params(
-        const char *model_path,
-        const char *lora_model_dir,
-        const char *vae_path,
-        int16_t n_threads,
-        enum sd_type_t wType,
-        enum rng_type_t rng_type,
-        enum schedule_t schedule
-) {
-    auto *params = new(new_sd_ctx_go_params);
-    params->model_path = model_path;
-    params->lora_model_dir = lora_model_dir;
-    params->vae_path = vae_path;
-    params->n_threads = n_threads;
-    params->wType = wType;
-    params->rng_type = rng_type;
-    params->schedule = schedule;
-
-    // Defaults
-    params->taesd_path = "";
-    params->control_net_path = "";
-    params->embed_dir = "";
-    params->id_embed_dir = "";
-    params->vae_decode_only = false;
-    params->vae_tiling = false;
-    params->free_params_immediately = false;
-    params->keep_clip_on_cpu = false;
-    params->keep_control_net_cpu = false;
-    params->keep_vae_on_cpu = false;
-
-    return params;
-}
-
-void new_sd_ctx_params_set(
-        new_sd_ctx_go_params *params,
-        const char *taesd_path,
-        const char *control_net_path,
-        const char *embed_dir,
-        const char *id_embed_dir,
-        bool vae_decode_only,
-        bool vae_tiling,
-        bool free_params_immediately,
-        bool keep_clip_on_cpu,
-        bool keep_control_net_cpu,
-        bool keep_vae_on_cpu
-) {
-    params->taesd_path = taesd_path;
-    params->control_net_path = control_net_path;
-    params->embed_dir = embed_dir;
-    params->id_embed_dir = id_embed_dir;
-    params->vae_decode_only = vae_decode_only;
-    params->vae_tiling = vae_tiling;
-    params->free_params_immediately = free_params_immediately;
-    params->keep_clip_on_cpu = keep_clip_on_cpu;
-    params->keep_control_net_cpu = keep_control_net_cpu;
-    params->keep_vae_on_cpu = keep_vae_on_cpu;
 }
--- stable-diffusion-abi.h
+++ stable-diffusion-abi.h
@@ -21,26 +21,6 @@
 extern "C" {
 #endif
 
-// STABLE_DIFFUSION_API void set_image_data(sd_image_t* image, uint8_t* data);
-//
-// STABLE_DIFFUSION_API void set_image_width(sd_image_t* image, uint32_t width);
-//
-// STABLE_DIFFUSION_API void set_image_height(sd_image_t* image, uint32_t height);
-//
-// STABLE_DIFFUSION_API void set_image_channel(sd_image_t* image, uint32_t channel);
-
-STABLE_DIFFUSION_API uint8_t *get_image_data(const sd_image_t *images, int index);
-
-STABLE_DIFFUSION_API uint32_t get_image_width(const sd_image_t *images, int index);
-
-STABLE_DIFFUSION_API uint32_t get_image_height(const sd_image_t *images, int index);
-
-STABLE_DIFFUSION_API uint32_t get_image_channel(const sd_image_t *images, int index);
-
-STABLE_DIFFUSION_API void sd_images_free(const sd_image_t *images);
-
-STABLE_DIFFUSION_API void sd_image_free(sd_image_t *image);
-
 STABLE_DIFFUSION_API sd_image_t *upscale_go(
         upscaler_ctx_t *ctx,
         uint32_t upscale_factor,
@@ -69,30 +49,6 @@ typedef struct {
     bool keep_control_net_cpu;
     bool keep_vae_on_cpu;
 } new_sd_ctx_go_params;
-
-STABLE_DIFFUSION_API new_sd_ctx_go_params *new_sd_ctx_params(
-        const char *model_path,
-        const char *lora_model_dir,
-        const char *vae_path,
-        int16_t n_threads,
-        enum sd_type_t wtype,
-        enum rng_type_t rng_type,
-        enum schedule_t schedule
-);
-
-void new_sd_ctx_params_set(
-        new_sd_ctx_go_params *params,
-        const char *taesd_path,
-        const char *control_net_path,
-        const char *embed_dir,
-        const char *id_embed_dir,
-        bool vae_decode_only,
-        bool vae_tiling,
-        bool free_params_immediately,
-        bool keep_clip_on_cpu,
-        bool keep_control_net_cpu,
-        bool keep_vae_on_cpu
-);
 
 STABLE_DIFFUSION_API sd_ctx_t *new_sd_ctx_go(new_sd_ctx_go_params *context_params);
 
